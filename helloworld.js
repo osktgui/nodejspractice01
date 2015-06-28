e0140bdd4a504149113d53c83534f4c3b1a4072e
luisoscategui <luis.oscategui@xurface.com>
2015-06-28 00:47:44 -0500

    files

--- helloworld.js
+++ helloworld.js
@@ -2,9 +2,23 @@ var http = require('http');
 
 
 http.createServer(function(req, res){
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Esto es un texto plano');
 
+	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+
+	switch(path){
+		case '':
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end('<h3>Path: Home</h3>');		
+			break;
+		case '/about':
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end('<h3>Path: about</h3>');
+			break;
+		default:
+			res.writeHead(404, { 'Content-Type': 'text/html' });
+			res.end('<h3>No found</h3>');
+			break;
+	}
 }).listen(3000);
 
 console.log('Server started on http://localhost:3000; Press Ctrl - C on terminate;');
