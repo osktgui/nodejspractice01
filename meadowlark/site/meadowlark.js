9db78abe9328683d778ae2022cae7c597aed923f
luisoscategui <luis.oscategui@xurface.com>
2015-06-28 01:26:54 -0500

    scaffolding

---
+++ meadowlark/site/meadowlark.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.type('text/plain');
+	res.send('Texto Plano');
+});
+
+//404 - not found
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not found');
+});
+
+//500 - Internal Error
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Internal Error');
+});
+
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; Press Ctrl - C to terminate....');
+});
