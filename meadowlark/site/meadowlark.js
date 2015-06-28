76bdd151f011ebbbf2e79786cad39e7ee149da14
luisoscategui <luis.oscategui@xurface.com>
2015-06-28 01:39:40 -0500

    Chapter 02 - View and Layouts

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -3,10 +3,32 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.set('view options', {
+	layout: false
+});
 
 app.get('/', function(req, res){
+	res.render('main', {
+		title: 'Main Page'
+	});
+});
+
+app.get('/about/contact', function(req, res){
 	res.type('text/plain');
-	res.send('Texto Plano');
+	res.send('About - Contact');
+});
+
+app.get('/about/directions', function(req, res){
+	res.type('text/plain');
+	res.send('About - Directions');
+});
+
+app.get('/about*', function(req, res){
+	res.render('about', {
+		title: 'About Page'
+	});
 });
 
 //404 - not found
