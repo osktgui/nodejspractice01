932ee46e4f1f4374780f5e17bc769564338f6398
luisoscategui <luis.oscategui@xurface.com>
2015-06-29 10:35:14 -0500

    chapter 08 - Handling with Express

--- meadowlark/site/meadowlark.js
+++ meadowlark/site/meadowlark.js
@@ -12,7 +12,9 @@ app.set('view options', {
 
 //if( app.thing == null ) console.log( 'bleat!' ); JHint
 
+
 app.use(express.static(__dirname + '/public')); //Middleware for static files and views
+app.use(require('body-parser')()); // Parse Url-endoce body.
 
 app.use(function(req, res, next){
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
@@ -53,6 +55,20 @@ app.get('/tours/request-group-rate', function(req, res){
 	res.render('tours/request-group-rate', {
 		title: 'Request Group Rate'
 	});
+});
+
+app.get('/newsletter', function(req, res){
+	res.render('newsletter', {
+		csrf: 'CSRF token goes here'
+	});
+});
+
+app.post('/process', function(req, res){
+	console.log('Form (from querystring): ' + req.query.form); 
+	console.log('CSRF token (from hidden form field): ' + req.body._csrf); 
+	console.log('Name (from visible form field): ' + req.body.name); 
+	console.log('Email (from visible form field): ' + req.body.email); 
+	res.redirect(303, '/thank-you');
 });
 
 //404 - not found
